81115403d68bdbb2844b0ad9656f7e7ec302a377
John Wang <johnz.wang@outlook.com>
2024-06-13 16:55:20 +0200

    Wrote digit

--- parse.pl
+++ parse.pl
@@ -1,16 +1,34 @@
 :- set_prolog_flag(double_quotes, chars).
+
+:- use_module(library(dcgs)).
+:- use_module(library(lists)).
+:- use_module(library(format)).
+
+a(a) --> [a].
+a(b) --> [b].
+
 remove_comments([]) --> [].
 remove_comments([X | Rest]) --> [X], { X \= '#' }, remove_comments(Rest).
-remove_comments(Rest) --> ['#'], comment, remove_comments(Rest).
+remove_comments(Rest) --> ['#'], seq(A), {maplist(\=('\n'), A)},  ['\n'], remove_comments(Rest).
 
-comment --> ['\n'].
-comment --> [X], { X \= '\n' }, comment.
+star(_, []) --> [].
+star(A, [X | Rest]) --> call(A, X), star(A, Rest).
+
+
+%% https://penman.readthedocs.io/en/latest/notation.html#formal-grammar
+
+symbol([X | Rest]) --> name_char(X), name_char_star(Rest).
+role(X) --> [':'], name_char_star(X).
+% cannot find where alignement is being used
+% alignment() --> ['~'], ((alphanum(X), ('.' | [] ))   | []), digit_plus, comma_digit_star.
+string(X) --> ['"'], seq(X), {maplist(\=('\n'), X)}, ['"'].
+name_char(X) --> [X], { \+ member(X, " \n\t\r\f\v()/:~")} .
+digit(X) --> [X], { char_code('0', Z), char_code('9', N), char_code(X, Xcode), Z =< Xcode, Xcode =< N } .
 
 
 
-% Helper predicate to process input and output
-remove_comments_from_string(Input, Output) :-
-    string_codes(Input, Codes),
-    phrase(remove_comments(X), Codes, CleanCodes),
-    print(X),
-    string_codes(Output, CleanCodes).
+
+
+
+
+% node(node(Var, Label, Relations)) --> ['('], variable(V), nodelabel(Label), relations, [')'].
