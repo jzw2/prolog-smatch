f0b153ef3dd4317537a064d80b9a0700e5c0d1ad
John Wang <johnz.wang@outlook.com>
2024-06-14 09:29:57 +0200

    Finished copying from penman

--- parse.pl
+++ parse.pl
@@ -16,14 +16,40 @@ star(A, [X | Rest]) --> call(A, X), star(A, Rest).
 
 
 %% https://penman.readthedocs.io/en/latest/notation.html#formal-grammar
+%%
+%%
+
+node(node(V, Label, R)) --> ['('], variable(V),
+                            (nodelabel(Label)
+                               | ([], {Label = none})),
+                            relation_star(R), [')'].
+nodelabel(label(C, A)) --> ['/'], concept(C), (alignment(A) | ([], {A = none})).
+concept(concept(C)) --> constant(C).
+relation(relation(R, A, V)) --> role(R),
+                                (alignment(A)
+                                   | ([], {A = none})),
+                                (node(V) |
+                                    (atom(V) % I'm going to ignore the alignment for now
+                                      , ([] | alignment(_))
+                                    )) .
+atom(atom(X)) --> variable(X) | constant(X).
+constant(constant(X)) --> string(X) | symbol(X).
+variable(variable(X)) --> symbol(X).
 
 symbol([X | Rest]) --> name_char(X), name_char_star(Rest).
 role(X) --> [':'], name_char_star(X).
 % cannot find where alignement is being used
-% alignment() --> ['~'], ((alphanum(X), ('.' | [] ))   | []), digit_plus, comma_digit_star.
+% alignment() --> ['~'], ((alphanum(X), (['.'] | [] ))   | []), digit_plus, comma_digit_star.
+alignment(X) --> ['~'],
+                ((alphanum(X), (['.'] | [] ))   | []),
+                digit_plus(X),
+                comma_digit_star.
 string(X) --> ['"'], seq(X), {maplist(\=('\n'), X)}, ['"'].
 name_char(X) --> [X], { \+ member(X, " \n\t\r\f\v()/:~")} .
-digit(X) --> [X], { char_code('0', Z), char_code('9', N), char_code(X, Xcode), Z =< Xcode, Xcode =< N } .
+name_char_star([]) --> [].
+name_char_star([X | Rest]) --> name_char(X), name_char_star(Rest).
+
+digit(X) --> [X], { member(X, "1234567890")} .
 
 
 
