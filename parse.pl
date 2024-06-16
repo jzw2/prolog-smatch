53c2e02d68cf92dc61031999aa18315185c4da25
John Wang <johnz.wang@outlook.com>
2024-06-16 15:05:18 +0200

    Create parse from lex

--- parse.pl
+++ parse.pl
@@ -26,7 +26,7 @@ node(node(V, Label, R)) --> ['('], variable(V),
                             relation_star(R), [')'].
 nodelabel(label(C, A)) --> ['/'], concept(C), (alignment(A) | ([], {A = none})).
 concept(concept(C)) --> constant(C).
-relation(relation(R, A, V)) --> role(R),
+relation(relation(R, A, V)) --> [role(R)],
                                 (alignment(A)
                                    | ([], {A = none})),
                                 (node(V) |
@@ -35,9 +35,9 @@ relation(relation(R, A, V)) --> role(R),
                                     )) .
 relation_star([]) --> [].
 relation_star([X | Rest]) --> relation(X), relation_star(Rest) .
-atom(atom(X)) --> variable(X) | constant(X).
-constant(constant(X)) --> string(X) | symbol(X).
-variable(variable(X)) --> symbol(X).
+atom(atom(X)) --> variable(X), ! | constant(X).
+constant(constant(X)) --> [string(X)] | [symbol(X)].
+variable(variable(X)) --> [symbol(X)].
 
 
 symbol(symbol([X | Rest])) --> name_char(X), name_char_star(Rest).
@@ -61,10 +61,10 @@ lex(X) --> ([' '] | ['\n']), lex(X).
 lex([X | Rest]) --> (string(X) | alignment(X) | role(X) | symbol(X) | [X], { member(X, "/()") }), lex(Rest).
 
 
-
 sent1("(w / want-01 :ARG0 (b / boy) :ARG1 (b2 / believe-01 :ARG0 (g / girl) :ARG1 b))").
 
 sent2("(p / person :domain (b / boy) :ARG0-of (w / work-01 :manner (h / hard)))").
+sent3(" (b / bear-02 :ARG1 (p / poet :name (n / name :op1 \"William\" :op2 \"Shakespeare\")) :location (c / city :name (n2 / name :op1 \"Stratford-upon-Avon\"))) ").
 test1(L) :- phrase_from_file(remove_comments(Y), "test_input1.txt"), phrase(lex(L), Y).
 
 
