25ace599d40e7731ec64249f42a47dac8a91a773
John Wang <johnz.wang@outlook.com>
2024-06-23 16:09:48 +0200

    Add precision, recall, f1

--- smatch.pl
+++ smatch.pl
@@ -1,5 +1,6 @@
 :- use_module(parse).
 :- use_module(library(lists)).
+:- use_module(library(ordsets)).
 
 boy1(X) :- triples_from_file("boy1.txt", X).
 boy2(X) :- triples_from_file("boy2.txt", X).
@@ -18,3 +19,19 @@ var_mapping(Domain, [Y | RangeRest], [X-Y | Rest]) :-
 
 variables(Triples, Variables) :-
     setof(X, var_in_triples(Triples, X), Variables).
+
+intersection(Left, Right, Amount) :-
+    ord_intersection(Left, Right, Intersect, _),
+    length(Intersect, Amount).
+
+precision(Left, Right, Score) :-
+    intersection(Left, Right, Numerator),
+    length(Left, Denominator),
+    Score is Numerator / Denominator.
+
+recall(Left, Right, Score) :- precision(Right, Left, Score).
+
+f1(Left, Right, Score) :-
+    precision(Left, Right, P),
+    recall(Left, Right, R),
+    Score is 2 * (P * R) / (P + R).
