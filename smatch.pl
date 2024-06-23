eaa8b47aabe33b444e8a771f6f880bb5357aa09a
John Wang <johnz.wang@outlook.com>
2024-06-23 15:36:34 +0200

    Add variables

--- smatch.pl
+++ smatch.pl
@@ -1,5 +1,20 @@
 :- use_module(parse).
-
+:- use_module(library(lists)).
 
 boy1(X) :- triples_from_file("boy1.txt", X).
 boy2(X) :- triples_from_file("boy2.txt", X).
+
+
+var_in_triples(Triples, X) :-
+    member(triple(X, _, _), Triples) ;
+    member(triple(_, _, variable(X)), Triples).
+
+
+var_mapping(_, [], []).
+var_mapping(Domain, [Y | RangeRest], [X-Y | Rest]) :-
+    select(X, Domain, DomainRest),
+    var_mapping(DomainRest, RangeRest, Rest).
+
+
+variables(Triples, Variables) :-
+    setof(X, var_in_triples(Triples, X), Variables).
